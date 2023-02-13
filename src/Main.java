705e5b16ea2e871e0c97a4b350106b27b0f3b95d
Marina Patrusheva <patrusheva_m@list.ru>
2023-02-13 20:40:15 +0500

    Среднее значение

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         SalesManager manager = new SalesManager(new int[]{23, 24, 56, 1});
         System.out.println(manager.max());
+        System.out.println(manager.average());
 
     }
 }
