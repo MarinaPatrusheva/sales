b8085875b835eeaf772e69a14a73fdb3653b8feb
Marina Patrusheva <patrusheva_m@list.ru>
2023-02-13 20:36:54 +0500

    Значения в long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager manager = new SalesManager(new int[]{23, 24, 56, 1});
+        SalesManager manager = new SalesManager(new long[]{23, 24, 56, 1, 24, 77, 54});
         System.out.println(manager.max());
 
     }
